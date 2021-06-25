4184d8a156366b67b091564515a592f255d115c5
Lukas Steiner <lukas.steiner@steinheilig.de>
2021-06-26 00:36:36 +0200

    fix bootstrap-auth k3s namespace

--- bootstrap-auth/outputs.tf
+++ bootstrap-auth/outputs.tf
@@ -3,7 +3,7 @@ data "shell_script" "ca" {
     read = <<-EOT
 timeout "${var.ca_shell_script_timeout}" bash -c ' \
   until \
-    kubectl --server "${var.k3s_url}" --insecure-skip-tls-verify --token "${var.token}" \
+    kubectl --server "${var.k3s_url}" --insecure-skip-tls-verify --token "${var.token}" --namespace default \
       get secret -o jsonpath="{.items[?(@.type==\"kubernetes.io/service-account-token\")].data}" | grep "ca.crt"
   do
     sleep 1
