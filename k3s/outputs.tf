afa14bbbc403a7e2f4b26981d2020dd3763b8a9e
Lukas Steiner <lukas.steiner@steinheilig.de>
2021-10-30 17:31:21 +0200

    add k3s and cni plugins version variable

--- k3s/outputs.tf
+++ k3s/outputs.tf
@@ -1,3 +1,11 @@
+locals {
+  k3s_bootstrap_manifest_b64 = var.bootstrap_token_id != null ? base64encode(
+    templatefile("${path.module}/cloud-init/bootstrap-token.yaml", {
+      token_id     = var.bootstrap_token_id
+      token_secret = var.bootstrap_token_secret
+  })) : ""
+}
+
 output "user_data" {
   value = templatefile("${path.module}/cloud-init/k3s.yml", {
     custom_cloud_config_write_files = var.custom_cloud_config_write_files
@@ -5,18 +13,17 @@ output "user_data" {
     ip                              = var.k3s_ip
     external_ip                     = var.k3s_external_ip
     persistent_volume_dev           = var.persistent_volume_dev
-    k3s_server                      = length(regexall("^server.*", var.install_k3s_exec)) > 0
-    k3s_config = base64encode(<<EOT
-K3S_TOKEN=${var.cluster_token}
-K3S_URL=${var.k3s_url}
-INSTALL_K3S_EXEC="${var.install_k3s_exec}"
-EOT
+    k3s_config = base64encode(<<-EOT
+      K3S_TOKEN=${var.cluster_token}
+      K3S_URL=${var.k3s_url}
+      INSTALL_K3S_EXEC="${var.install_k3s_exec}"
+      %{ if var.k3s_version != null ~}
+      INSTALL_K3S_VERSION=${var.k3s_version}
+      %{ endif ~}
+      EOT
     )
-    k3s_bootstrap_manifest_b64 = var.bootstrap_token_id != null ? base64encode(
-      templatefile("${path.module}/cloud-init/bootstrap-token.yaml", {
-        token_id     = var.bootstrap_token_id
-        token_secret = var.bootstrap_token_secret
-    })) : ""
+    k3s_bootstrap_manifest_b64 = local.k3s_bootstrap_manifest_b64
+    cni_plugins_version        = var.cni_plugins_version
   })
   sensitive = true
 }
